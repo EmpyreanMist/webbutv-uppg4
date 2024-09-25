ebde4d78c14d9a6da76d3c61466c318d7ffa1c91
Christian Fryksten <christianfryksten1@gmail.com>
2024-09-25 09:30:40 +0200

    Changed the alert method to DOM manipulation instead

--- script.js
+++ script.js
@@ -2,62 +2,70 @@ const addBtn = document.querySelector("#addBtn"); // Declaring the addButton
 const clearBtn = document.querySelector("#clearBtn"); //Declaring the clearBtn
 const ul = document.querySelector("#ul"); //Declaring the unordered list
 const input = document.querySelector("#input"); //Declaring the input button
-let tasks = []; //Declaring an array to hold the tasks
+const header = document.querySelector("#header");
+const tasks = []; //Declaring an array to hold the tasks
 
 clearBtn.addEventListener("click", clearAll); //Calling clearAll function when clicking on clearBtn
-addBtn.addEventListener("click", addTaskToArray); //Calling addTaskToArray function when clicking 
-
-
+addBtn.addEventListener("click", addTaskToArray); //Calling addTaskToArray function when clicking
 
 function addTaskToArray() {
-  const task = input.value; //Holds the text in the input 
+  const task = input.value; //Holds the text in the input
 
-  if (task !== "") { //If the text isn't empty we make an object and push it into the tasks array
+  if (task !== "") {
+    //If the text isn't empty we make an object and push it into the tasks array
     const newTask = {
       name: task,
-      completed: false
+      completed: false,
     };
-    tasks.push(newTask); 
+    header.innerHTML = "Christians Todo List!";
+    tasks.push(newTask);
     input.value = ""; //Resetting the text in the input
-    displayTasks(); 
+    displayTasks();
     updateTaskCount(); //Function for updating total tasks
   } else {
-    alert("Don't be lazy :)"); //Warning box if you try to add an empty text
+    header.innerHTML = "Don't be lazy!";
   }
 }
-
 
 function displayTasks() {
   ul.innerHTML = ""; //Clearing the list before adding the updated one
 
-  for (let i = 0; i < tasks.length; i++) { //Looping through the array and creating new li
+  for (let i = 0; i < tasks.length; i++) {
+    //Looping through the array and creating new li
     const task = tasks[i];
     const li = document.createElement("li");
     li.textContent = task.name;
 
-    if (task.completed) { //Adding css class if it's completed
+    if (task.completed) {
+      //Adding css class if it's completed
       li.classList.add("completed");
     }
 
     //Making event for click when pressing on a todo
-    li.addEventListener("click", (function(index) {
-      return function() {
-        toggleTaskCompletion(index);
-      };
-    })(i));
+    li.addEventListener(
+      "click",
+      (function (index) {
+        return function () {
+          toggleTaskCompletion(index);
+        };
+      })(i)
+    );
 
-   //Making a trash icon that can be used to remove todos
+    //Making a trash icon that can be used to remove todos
     const deleteBtn = document.createElement("button");
     deleteBtn.innerHTML = "🗑️";
     deleteBtn.classList.add("delete-btn");
 
     // Making another click event
-    deleteBtn.addEventListener("click", (function(index) {
-      return function(event) {
-        event.stopPropagation(); 
-        deleteTask(index);
-      };
-    })(i));
+    deleteBtn.addEventListener(
+      "click",
+      (function (index) {
+        return function (event) {
+          event.stopPropagation();
+          deleteTask(index);
+        };
+      })(i)
+    );
 
     li.appendChild(deleteBtn); // Adding the deleteBtn icon
     ul.appendChild(li); //Adding the list inside of ul
@@ -67,8 +75,8 @@ function displayTasks() {
 function updateTaskCount() {
   let unfinishedTasksCount = 0;
   let finishedTasksCount = 0;
-  for(let i = 0; i < tasks.length; i++) {
-    if(tasks[i].completed) {
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].completed) {
       finishedTasksCount++;
     } else {
       unfinishedTasksCount++;
@@ -78,14 +86,14 @@ function updateTaskCount() {
   const unfinishedCountEl = document.querySelector("#taskCount");
   const finishedCountEl = document.querySelector("#finishedCount");
 
-  unfinishedCountEl.textContent = "Unfinished tasks: " + unfinishedTasksCount; 
+  unfinishedCountEl.textContent = "Unfinished tasks: " + unfinishedTasksCount;
   finishedCountEl.textContent = "Finished tasks: " + finishedTasksCount;
 }
 
 //For marking completed
 function toggleTaskCompletion(index) {
-  tasks[index].completed = !tasks[index].completed; 
-  displayTasks(); 
+  tasks[index].completed = !tasks[index].completed;
+  displayTasks();
   updateTaskCount();
 }
 
